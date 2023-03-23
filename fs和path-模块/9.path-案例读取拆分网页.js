6d67bc3b1c31f7a1bad067001adff468264a9d41
dyc <dingyangcai@163.com>
2023-03-23 22:56:51 +0800

    ss

--- fs和path-模块/9.path-案例读取拆分网页.js
+++ fs和path-模块/9.path-案例读取拆分网页.js
@@ -15,6 +15,7 @@ fs.readFile(path.join(__dirname, './files/index.html'), function(err, dataStr){
     // 读取成功 调用函数
     resloveCSS(dataStr)
     resloveJS(dataStr)
+    resloveHTML(dataStr)
 })
 
 
@@ -27,7 +28,7 @@ function resloveCSS(htmlStr){
     fs.writeFile(path.join(__dirname, './files/index.css', newCSS, function(err){
         if(err) return console.log(err.message + '文件写入失败！');
 
-        console.log('文件写入成功！');
+        console.log('css文件写入成功！');
     }))
 }
 
@@ -38,9 +39,21 @@ function resloveJS(htmlStr){
     // 提取到的数据进行拼接
     const newJS = r2[0].replace('<script>', '').replace('</script>', '')
     // 写入文件
-    fs.writeFile(path.join(__dirname, './files/index.js'), newJS, function(err){
+    fs.writeFile(path.join(__dirname, './files/index.js'), newJS,(err)=>{
         if(err) return console.log('写入文件失败' + err.message);
 
-        console.log('文件写入失败');
+        console.log('js文件写入失败');
     })
 }
+
+
+// 定义提取html方法
+function resloveHTML(htmlStr) {
+    // 将style标签和script标签替换成 外联形式
+    const newHTML = htmlStr.replace(regStyle, '<link rel="stylesheet" href="./files/index.css').replace(regScript, '<script src="./files/index.js"></script>')
+    
+    // 写入文件
+    fs.writeFile(path.join(__dirname, './indexhtml'), newHTML, function(err){
+        if(err) return console.log('html文件写入失败' + err.message);
+    })
+}   
